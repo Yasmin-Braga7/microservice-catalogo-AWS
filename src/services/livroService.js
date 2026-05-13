4945bd8b5b55f0450045f7afbeefabd4d6bb0e03
Yasmin-Braga7 <yasminbraga161@gmail.com>
2026-05-13 11:27:31 -0300

    alteração no livro service

--- src/services/livroService.js
+++ src/services/livroService.js
@@ -4,18 +4,30 @@ const rabbitmq = require('../config/rabbitmq');
 
 async function listarLivros() {
     return await prisma.livro.findMany({
-        include: { autores: true, generos: true }
+        // Atualizado para trazer os dados reais do Autor e Gênero através da tabela de ligação
+        include: {
+            autores: { include: { autor: true } },
+            generos: { include: { genero: true } }
+        }
     });
 }
 
 async function buscarLivroPorId(id) {
     return await prisma.livro.findUnique({
         where: { id: Number(id) },
-        include: { exemplares: true, autores: true, generos: true }
+        include: {
+            exemplares: true,
+            autores: { include: { autor: true } },
+            generos: { include: { genero: true } }
+        }
     });
 }
 
 async function cadastrarLivro(dados) {
+    // Pegando os arrays de IDs vindos da requisição (Postman). Se vier vazio, garante que não dá erro.
+    const autoresIds = dados.autores || [];
+    const generosIds = dados.generos || [];
+
     const novoLivro = await prisma.livro.create({
         data: {
             titulo: dados.titulo,
@@ -25,9 +37,26 @@ async function cadastrarLivro(dados) {
             sinopse: dados.sinopse,
             numeroPaginas: dados.numeroPaginas,
             idioma: dados.idioma,
-            status: 1
-            // Nota: Caso venham os arrays de IDs na requisição, 
-            // você pode conectar autores e gêneros aqui usando o "connect".
+            status: 1,
+
+            // Aqui fazemos a ligação com os Autores
+            autores: {
+                create: autoresIds.map(idAutor => ({
+                    autor: { connect: { id: idAutor } }
+                }))
+            },
+
+            // Aqui fazemos a ligação com os Gêneros
+            generos: {
+                create: generosIds.map(idGenero => ({
+                    genero: { connect: { id: idGenero } }
+                }))
+            }
+        },
+        // Opcional: já retorna o livro recém-criado com os dados dos autores e gêneros embutidos
+        include: {
+            autores: { include: { autor: true } },
+            generos: { include: { genero: true } }
         }
     });
 
@@ -56,4 +85,4 @@ async function alterarStatusLivro(id, status) {
     return livroAtualizado;
 }
 
-module.exports = { listarLivros ,buscarLivroPorId, cadastrarLivro, alterarStatusLivro };
+module.exports = { listarLivros, buscarLivroPorId, cadastrarLivro, alterarStatusLivro };
